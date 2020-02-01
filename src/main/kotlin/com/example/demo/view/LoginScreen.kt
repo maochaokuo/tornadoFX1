930a998bbe9e5f2abf90a56520afbf8d8f38a292
Thomas Mao <tm1813@gmail.com>
2020-02-01 12:06:12 +0800

    initial myapp TornadoFX project

---
+++ src/main/kotlin/com/example/demo/view/LoginScreen.kt
@@ -0,0 +1,17 @@
+package com.example.demo.view
+
+import javafx.scene.layout.VBox
+import tornadofx.*
+
+class LoginScreen : View() {
+    override val root= VBox()
+
+    init {
+        with (root) {
+
+            hbox {
+                
+            }
+        }
+    }
+}
